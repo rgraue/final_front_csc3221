f318b3e10d7f4292337fc9d69207caa06e7c1a24
Ryan Graue <grauer@spu.edu>
2021-06-11 21:51:15 -0700

    public pages, nav bar, and author/book table retrieval

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+const port = process.env.port || 3000
+
+app.use(express.static(__dirname+'/public'))
+
+app.listen(port, '0.0.0.0', function ServerListener(){
+    console.log(port);
+});
